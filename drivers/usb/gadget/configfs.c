f4723f66509e90a8f4ea0b1ef4bad003617f7faa
Gianmarco <gianmarcoscarano@gmail.com>
2020-02-20 15:14:19 +0000

    pyxis: fix unused configfs function
    Signed-off-by: Gianmarco <gianmarcoscarano@gmail.com>

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -1520,6 +1520,7 @@ static void configfs_composite_unbind(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
 
+#ifndef CONFIG_USB_CONFIGFS_UEVENT
 static int configfs_composite_setup(struct usb_gadget *gadget,
 		const struct usb_ctrlrequest *ctrl)
 {
@@ -1544,6 +1545,7 @@ static int configfs_composite_setup(struct usb_gadget *gadget,
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 	return ret;
 }
+#endif
 
 static void configfs_composite_disconnect(struct usb_gadget *gadget)
 {
